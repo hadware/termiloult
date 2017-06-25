3f6a0a49ef16355cc92e24c77f58c578a53c442e
Pr Feurisson <kaname.madoka@protonmail.com>
2017-06-25 03:03:11 +0200

    First draft for an asynchronous sound player.

--- test_multiple_play.py
+++ test_multiple_play.py
@@ -1,29 +1,109 @@
-class WavPlayer:
-    """A sound player"""
-    chunk = 1024
+import wave
+from asyncio import get_event_loop, ensure_future, sleep
+from queue import Queue
 
-    def __init__(self, file):
+from pyaudio import PyAudio, paContinue, paComplete
+
+
+class AWavPlayer:
+    """ Asynchronous wav player based on portaudio """
+
+    wave = None # wave.Wave_read object
+    player = None
+    sample_width = 0
+    _queue = None
+    _stop_next = False
+    _done = None # future signaling end of play
+
+    def __init__(self, fd=None, loop=None):
         """ Init audio stream """
-        self.wf = wave.open(file, 'rb')
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(
-            format = self.p.get_format_from_width(self.wf.getsampwidth()),
-            channels = self.wf.getnchannels(),
-            rate = self.wf.getframerate(),
-            output = True
+        self.player = PyAudio()
+        self.loop = loop or get_event_loop()
+        self._queue = Queue()
+
+        if fd:
+            self.play_file(fd)
+            self.close()
+
+    def _worker(self, in_data, frame_count, time_info, status):
+        # Caution, this callback is running into a separate thread!
+        if self._stop_next:
+            self._stop_next = False
+            self.loop.call_soon_threadsafe(self._done.set_result, None)
+            return b'', paComplete
+
+        expected_size = frame_count * self.sample_width
+
+        frames = list()
+        for _ in range(frame_count):
+            frame = self._queue.get()
+            frames.append(frame)
+            self._queue.task_done()
+            if not frame:
+                self._stop_next = True
+                break
+
+        data = self._frame_pad(b''.join(frames), frame_count)
+        return data, paContinue
+
+    def _frame_pad(self, frame, frame_count=1):
+        """ Pad a frame for PyAudio which else would stop """
+        return frame.ljust(self.sample_width * frame_count, b'\x00')
+
+    def enqueue(self, frame):
+        """ Enqueue a frame to be played as soon as possible.
+
+        If you pass an empty byte array the player will stop once
+        it reaches it.
+        """
+        self._queue.put_nowait(frame)
+
+    async def play(self, channels, rate, sample_width):
+        self._done = self.loop.create_future()
+        self.sample_width = sample_width
+
+        wav_format = self.player.get_format_from_width(sample_width)
+        self.player.open(
+            format=wav_format,
+            channels=channels,
+            rate=rate,
+            output=True,
+            stream_callback=self._worker
         )
 
-    def play(self):
-        """ Play entire file """
-        data = self.wf.readframes(self.chunk)
-        while data != b'':
-            self.stream.write(data)
-            data = self.wf.readframes(self.chunk)
+        return await self._done
+
+    async def aplay_file(self, fd):
+        """ Play a file or a file-like object asynchronously
+
+        This will block while reading the file unless you pass a file-like
+        object already in memory.
+        """
+        self._done = self.loop.create_future()
+        wf = wave.open(fd, 'rb')
+
+        play_coro = self.play(wf.getnchannels(), wf.getframerate(),
+                              wf.getsampwidth())
+        play_task = ensure_future(play_coro, loop=self.loop)
+
+        for i in range(wf.getnframes()):
+            self.enqueue(wf.readframes(1))
+            await sleep(0)
+        self.enqueue(b'')
+
+        await play_task
+        wf.close()
+
+    def play_file(self, fd):
+        """ Play a file or a file-like object synchronously """
+        return self.loop.run_until_complete(self.aplay_file(fd))
 
     def close(self):
         """ Graceful shutdown """
-        self.stream.close()
-        self.p.terminate()
+        self.player.terminate()
 
-SOUND_FILE = "sample.wav"
 
+if __name__ == '__main__':
+    from sys import argv
+    fname = argv[1] if len(argv) > 1 else "sample.wav"
+    AWavPlayer(fname)
