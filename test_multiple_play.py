14be1c79bf545a448dc8958eb6b53c0f9be95667
Pr Feurisson <kaname.madoka@protonmail.com>
2017-07-02 13:08:02 +0200

    Log what is happening in the example for AudioSink.

--- test_multiple_play.py
+++ test_multiple_play.py
@@ -3,28 +3,36 @@ from time import sleep
 
 from audiosink import AudioSink
 
+demo = logging.getLogger('Demo')
+
 logging.basicConfig(level=logging.DEBUG)
+
 
 p = AudioSink()
 
 with open("sample.wav", 'rb') as f:
     a = f.read()
+demo.info("add the first track")
 p.add(a, "a")
 
 sleep(2)
 
 with open("sample.wav", 'rb') as f:
     b = f.read()
+demo.info("add a second track")
 p.add(b,"b")
 
 sleep(5)
 
+demo.info("remove the first track")
 p.remove("a")
 
 sleep(5)
 
+demo.info("lower the volume to 40%")
 p.volume = 40
 
 sleep(15)
 
+demo.info("close the AudioSink")
 p.close()
